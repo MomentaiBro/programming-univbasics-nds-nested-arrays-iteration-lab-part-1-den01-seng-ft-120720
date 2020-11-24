c57ce588eb09056eb696491d2ce2b193b87b1aa5
Alexander S <alx.sherwin@gmail.com>
2020-11-24 21:18:02 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -13,6 +13,8 @@ while row_index < array.length do
     element_index += 1 
   end
   row_index += 1
+  
+  array
 end
   
   # Given an Array of Arrays of Integers ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ):
